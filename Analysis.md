0f7c1985dc37b0135c006d16a690b5902c81ff1e
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-02-06 14:00:53 +0200

    add static&media settings

---
+++ Analysis.md
@@ -0,0 +1,28 @@
+product:
+    - name
+    - image
+    - price
+    - flag  [feature - new - sale]
+
+    - brand :
+        - brand
+        - image 
+        - slug
+
+    - sku 
+    - subtitle
+    - tags
+
+    - images :
+        - product
+        - image
+    - reviews :
+        - user
+        - product
+        - review
+        - rate
+        - data            
+
+    - descr
+    - specification
+    - slug    
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR/ "static",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
