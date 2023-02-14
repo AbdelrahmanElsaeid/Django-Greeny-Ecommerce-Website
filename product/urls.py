65010c0bc6deae90c558105f58cc2955f161e0b1
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-02-14 23:47:28 +0200

    add reviews

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductList, ProductDetail
+from .views import ProductList, ProductDetail ,add_review
 
 app_name = 'product'
 
 urlpatterns = [
     path('', ProductList.as_view(),name='product_list'),
-    path('<slug:slug>', ProductDetail.as_view(),name='product_detail')
+    path('<slug:slug>', ProductDetail.as_view(),name='product_detail'),
+    path('<slug:slug>/add-review',add_review ,name='add_review'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.views.generic import ListView,DetailView
 from .models import Product
+from .forms import ProductReviewForm
 # Create your views here.
 
 
@@ -13,3 +14,15 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model = Product    
+
+def add_review(request,slug):
+    product = Product.objects.get(slug = slug)
+    if request.method == "POST":
+        form = ProductReviewForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.user = request.user
+            myform.product = product
+            myform.save()
+    return redirect(f'/products/{product.slug}')        
+
