5a567a35a950e212e938781605467d23eab3d284
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-03-08 00:24:07 +0200

    add product images to produvt detail api

--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Brand, ProductReview
+from .models import Product, Brand, ProductReview, ProductImages
 
 
 
@@ -14,17 +14,26 @@ class ProductReviewSerializer(serializers.ModelSerializer):
         model = ProductReview
         fields = ['id','user','rate','review','data']
 
-class ProductSerializer(serializers.ModelSerializer):
-    #brand = BrandSerializer()
-    brand = serializers.StringRelatedField()
-    reviews = ProductReviewSerializer(source='product_review', many=True )
-    class Meta:
-        model = Product
-        fields = '__all__'
-
 
 class BrandDetailSerializer(serializers.ModelSerializer):
     product = BrandSerializer(source='product_brand', many=True)
     class Meta:
         model = Brand
         fields = '__all__'        
+
+
+class ProductImagesApi(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImages
+        fields = '__all__'   
+
+class ProductSerializer(serializers.ModelSerializer):
+    #brand = BrandSerializer()
+    brand = serializers.StringRelatedField()
+    reviews = ProductReviewSerializer(source='product_review', many=True)
+    images = ProductImagesApi(source='product_images', many=True)
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+     
