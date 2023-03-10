f508c7b3f2a659d519ce2bccfbd98e3b0d05f924
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-03-10 14:44:42 +0200

    show cart in base

--- orders/cart_context_processor.py
+++ orders/cart_context_processor.py
@@ -3,8 +3,8 @@ from .models import Cart, CartDetail
 
 def get_or_create_cart(request):
     if request.user.is_authenticated:
-        cart = Cart.objects.get_or_create(user = request.user, status = 'Inprogress')
-        #cart_detail = CartDetail.objects.filter(cart = cart), 'cart_detail': cart_detail
-        return {'cart': cart}
+        cart,created = Cart.objects.get_or_create(user = request.user, status = 'Inprogress')
+        cart_detail = CartDetail.objects.filter(cart = cart)
+        return {'cart': cart , 'cart_detail': cart_detail}
     else:
         return {}
